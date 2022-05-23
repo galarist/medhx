bff6071066bced08e27bebfdbc31fa7ea3894f4b
Galarist <galamboscristovao@gmail.com>
2022-05-23 23:51:05 +1000

    allow multiple cors origin

--- controller/cors.php
+++ controller/cors.php
@@ -8,7 +8,14 @@ $content = file_get_contents('php://input');
 //convert to stdclass object
 $data = json_decode($content, true);
 if (!empty($_SERVER['HTTPS'])) {
-    header('Access-Control-Allow-Origin: https://medhx.netlify.app/');
+    $allowed_domains = [
+        "https://medhx.herokuapp.com/",
+        "https://medhx.netlify.app/"
+    ];
+
+    if (in_array($_SERVER['HTTP_ORIGIN'], $allowed_domains)) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+    }
 } else {
     header('Access-Control-Allow-Origin: http://localhost:1234');
 }
